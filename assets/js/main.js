5e55318e3d34cf49f3caae98c60b84abb7fe6d44
Cushione <maulerovak@gmail.com>
2022-10-23 12:19:52 +0000

    Add basic game functions
    - Assign values to cards
    - Shuffle cards
    - Check for matches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
+let cardValues = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
+let openCardValue = 0
+
+document.getElementById('game-container').addEventListener('click', handleClick);
+
+function handleClick(event) {
+    
+    let cards = Array.from(this.children);
+    let clickedCard = event.target.parentElement.parentElement;
+    let index = cards.indexOf(clickedCard);
+    console.log(index, cardValues[index]);
+    let currentCardValue = cardValues[index];
+
+
+    if (openCardValue === 0) {
+        openCardValue = currentCardValue
+    } else {
+        if (openCardValue === currentCardValue) {
+            console.log("Match found! YAY")
+        } else {
+            console.log("No match! :(")
+        }
+        openCardValue = 0
+
+    }
+}
+
 //temporary funtion for testing cards
 function flipCard(event) {
     console.log(event.target.parentElement.classList)
@@ -5,6 +32,15 @@ function flipCard(event) {
         event.target.parentElement.classList.remove("flip")
     } else {
         event.target.parentElement.classList.add("flip")
+    }
+}
+
+function shuffleCards() {
+    for (let i = cardValues.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cardValues[i];
+        cardValues[i] = cardValues[j];
+        cardValues[j] = temp;
     }
 }
 
@@ -17,9 +53,11 @@ function hide(id) {
 }
 
 function newGame() {
+    shuffleCards() 
+    console.log(cardValues);
     hide("menu-container");
     for (let i = 0; i<20; i++) {
-        document.getElementById("game-container").innerHTML+=`<div class="card" onclick="flipCard(event)">
+        document.getElementById("game-container").innerHTML+=`<div class="card">
         <div class="card-inner">
             <div class="card-front">
                 <p>card front</p>
