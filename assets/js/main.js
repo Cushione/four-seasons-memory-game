4182f97327aa1f2e00aa0ed5c9e568be0c9292f5
Cushione <maulerovak@gmail.com>
2022-10-23 10:08:28 +0000

    Add game cards with basic styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+//temporary funtion for testing cards
+function flipCard(event) {
+    console.log(event.target.parentElement.classList)
+    if (event.target.parentElement.classList.contains("flip")) {
+        event.target.parentElement.classList.remove("flip")
+    } else {
+        event.target.parentElement.classList.add("flip")
+    }
+}
+
 function show(id) {
     document.getElementById(id).classList.remove("hidden");
 }
@@ -8,6 +18,18 @@ function hide(id) {
 
 function newGame() {
     hide("menu-container");
+    for (let i = 0; i<20; i++) {
+        document.getElementById("game-container").innerHTML+=`<div class="card" onclick="flipCard(event)">
+        <div class="card-inner">
+            <div class="card-front">
+                <p>card front</p>
+            </div>
+            <div class="card-back">
+                <p>card back</p>
+            </div>
+        </div>
+    </div>`
+    }
     show("game-container");
 }
 
