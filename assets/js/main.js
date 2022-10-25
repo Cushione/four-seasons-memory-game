374cb7f0bdde47c6e927820075f99a54c7476a84
Cushione <maulerovak@gmail.com>
2022-10-25 17:35:48 +0000

    Add timer and score counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,11 @@ let cardValues = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
 let openCardIndex = -1
 let cardsToClose = []
 let selectedSeason = ''
-
-document.getElementById('game-container').addEventListener('click', handleClick);
+let time = 0
+let timer 
+let score = 0
+ 
+document.getElementById('game-board').addEventListener('click', handleClick);
 
 function handleClick(event) {
     if (cardsToClose.length) {
@@ -39,6 +42,10 @@ function handleClick(event) {
             console.log("Match found! YAY")
             openCard.classList.add("matched")
             clickedCard.classList.add("matched")
+            document.getElementById("game-score").innerHTML = `${++score}/10`
+            if (score === 10) {
+                clearInterval(timer)
+            }
         } else {
             console.log("No match! :(")
             cardsToClose = [clickedCard, openCard]
@@ -97,14 +104,18 @@ function newGame(season) {
     document.getElementById('game-container').classList.add(season)
     hide("menu-container");
     for (let i = 0; i < 20; i++) {
-        document.getElementById("game-container").innerHTML += `<div class="card">
+        document.getElementById("game-board").innerHTML += `<div class="card">
         <div class="card-inner">
             <div class="card-front"></div>
             <div class="card-back"></div>
         </div>
     </div>`
     }
+    document.getElementById("game-score").innerHTML = "0/10"
     show("game-container");
+    timer = setInterval(() => {
+        document.getElementById("game-timer").innerHTML = time++
+    }, 1000);
 }
 
 function selectTheme() {
