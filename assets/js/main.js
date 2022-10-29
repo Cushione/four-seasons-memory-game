92979cc4336b5705c1d8508be0c03f54b05355d8
Cushione <maulerovak@gmail.com>
2022-10-29 12:14:22 +0000

    Refactor and add comments to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Variables
 let cardValues = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
 let openCardIndex = -1
 let cardsToClose = []
@@ -8,6 +9,7 @@ let moves = 0
 let matches = 0
 let highScoreList = JSON.parse(localStorage.getItem("highScore")) || [];
 
+// Ids of the menu elements
 const MENU = {
     MAIN: "menu-buttons",
     THEME: "theme-buttons",
@@ -16,70 +18,133 @@ const MENU = {
     HOWTOPLAY: "how-to-play"
 }
 
-document.getElementById('game-board').addEventListener('click', handleClick);
+// Gameplay functions
 
-function handleClick(event) {
+// Add click event listener to the game board
+document.getElementById('game-board').addEventListener('click', handleCardClick);
+
+// Handle click on a card in the game
+function handleCardClick(event) {
+
+    // Prevent click if two cards are still open
     if (cardsToClose.length) {
         return
     }
 
+    // Find all the cards on the board
     let cards = Array.from(this.children);
+    // Find clicked card
     let clickedCard = event.target.parentElement.parentElement;
 
+    // Ignore clicks outside of a card or on an already matched card
     if (!clickedCard.classList.contains("card") || clickedCard.classList.contains("matched")) {
-        console.log("not a card or already matched")
         return
     }
 
+    // Find the index of the clicked card
     let index = cards.indexOf(clickedCard);
-    console.log(index, cardValues[index]);
 
+    // Ignore clicks on an already opened card
     if (openCardIndex !== -1 && openCardIndex === index) {
         console.log("same card")
         return
     }
 
-    let currentCardValue = cardValues[index];
-    flipCard(clickedCard, currentCardValue)
+    // Get the value of the clicked card
+    let clickedCardValue = cardValues[index];
+    // Flip over the clicked card
+    flipCard(clickedCard, clickedCardValue)
 
-
+    // If the clicked card is the first card of a move, store its index, otherwise check for match
     if (openCardIndex === -1) {
         openCardIndex = index
     } else {
-        let openCardValue = cardValues[openCardIndex]
-        let openCard = cards[openCardIndex]
+        // Check for a match and increment move counter
+        checkForMatch(cards, clickedCard, clickedCardValue)
         document.getElementById("game-moves").innerHTML = `${++moves}`
-
-        if (openCardValue === currentCardValue) {
-            console.log("Match found! YAY")
-            openCard.classList.add("matched")
-            clickedCard.classList.add("matched")
-            if (++matches === 10) {
-                document.getElementById("final-moves").innerHTML = moves
-                document.getElementById("final-time").innerHTML = time
-                clearInterval(timer)
-                checkForHighScore()
-                changeMenu(MENU.RESULT)
-                setTimeout(function () {
-                    show("menu-container")
-                    hide("game-container")
-                    document.getElementById("game-container").classList.remove(selectedSeason)
-                }, 2000)
-            }
-        } else {
-            console.log("No match! :(")
-            cardsToClose = [clickedCard, openCard]
-            setTimeout(function () {
-                closeOpenCards()
-            }, 1300)
-
-        }
+        // Remove index of the previously opened card
         openCardIndex = -1
+
+        // If all the cards are matched, end the game
+        if (matches === 10) {
+            endGame()
+        }
+    }
+}
+
+/** End the current game and display the result */
+function endGame() {
+    // Set the final time and number of moves on the game result screen
+    document.getElementById("final-moves").innerHTML = moves
+    document.getElementById("final-time").innerHTML = time
+    // Stop the timer
+    clearInterval(timer)
+    // Check if a new high score was achieved
+    checkForHighScore()
+    // Set the menu screen to the game result
+    changeMenu(MENU.RESULT)
+
+    // Show final board for 2 seconds, then display the game result
+    setTimeout(function () {
+        show("menu-container")
+        hide("game-container")
+        // Remove played season class from the game container
+        document.getElementById("game-container").classList.remove(selectedSeason)
+    }, 2000)
+}
+
+/** Compare a card to a previously opened card. */
+function checkForMatch(cards, clickedCard, clickedCardValue) {
+    // Find the value and html element of the already opened card
+    let openCardValue = cardValues[openCardIndex]
+    let openCard = cards[openCardIndex]
+
+    // Compare card with the previously opened card
+    if (openCardValue === clickedCardValue) {
+        // If cards match, leave them open and increment match counter
+        openCard.classList.add("matched")
+        clickedCard.classList.add("matched")
+        matches++
+    } else {
+        // If cards do not match, set them to close after 1.3 seconds 
+        cardsToClose = [clickedCard, openCard]
+        setTimeout(function () {
+            closeOpenCards()
+        }, 1300)
 
     }
 }
 
+/** Close two open, non matching cards */
+function closeOpenCards() {
+    for (let card of cardsToClose) {
+        flipCard(card)
+    }
+    cardsToClose = []
+}
+
+/** Flip a card and add image corresponding to the value if it is being opened */
+function flipCard(card, value) {
+    if (card.classList.contains("flipped")) {
+        // Close the card and remove the image afterwards if it is not already opened again
+        card.classList.remove("flipped")
+        setTimeout(function () {
+            if (!card.classList.contains("flipped")) {
+                card.children[0].children[1].style.setProperty('background-image', 'none')
+            }
+        }, 1000)
+    } else {
+        // Add image to the card back and then open it
+        card.children[0].children[1].style.setProperty('background-image', `url(assets/images/themes/${selectedSeason}/${value}.png)`)
+        card.classList.add("flipped")
+    }
+}
+
+// High score functions
+
+/** Check if the player achieved a new high score */
 function checkForHighScore() {
+    // Check if the player beat a previous high score
     for (let i = 0; i < highScoreList.length; i++) {
         if (highScoreList[i].time > time) {
             addToHighScore(i)
@@ -91,63 +156,52 @@ function checkForHighScore() {
             }
         }
     }
+    // Add score to the end if high score list is empty or previous scores were not beat
     addToHighScore(highScoreList.length)
 }
 
+/** Add score to the high score list at given index */
 function addToHighScore(index) {
+    // Add score to the list
     highScoreList.splice(index, 0, {
         time: time,
         moves: moves,
         season: selectedSeason
     })
+    // Reduce the list to five scores
     highScoreList = highScoreList.slice(0, 5)
+    // Save the high score list to local storage
     localStorage.setItem('highScore', JSON.stringify(highScoreList));
-    console.log(highScoreList, localStorage.getItem('highscore'))
 }
 
-function closeOpenCards() {
-    for (let card of cardsToClose) {
-        flipCard(card)
-    }
-    cardsToClose = []
-}
+// Game initialisation functions
 
-function flipCard(card, value) {
-    if (card.classList.contains("flipped")) {
-        card.classList.remove("flipped")
-        setTimeout(function () {
-            if (!card.classList.contains("flipped")) {
-                card.children[0].children[1].style.setProperty('background-image', 'none')
-            }
-        }, 1000)
-    } else {
-        card.children[0].children[1].style.setProperty('background-image', `url(assets/images/themes/${selectedSeason}/${value}.png)`)
-        card.classList.add("flipped")
-    }
-}
-
-function shuffleCards() {
-    for (let i = cardValues.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = cardValues[i];
-        cardValues[i] = cardValues[j];
-        cardValues[j] = temp;
-    }
-}
-
-function show(id) {
-    document.getElementById(id).classList.remove("hidden");
-}
-
-function hide(id) {
-    document.getElementById(id).classList.add("hidden");
-}
-
+/** Start a new game with the given season */
 function newGame(season) {
     selectedSeason = season
+    // Reset the game
     resetGame()
+    // Add the selected season class to the game container
     document.getElementById('game-container').classList.add(season)
+    // Show the game
     hide("menu-container");
+    show("game-container");
+}
+
+/** Reset all game elements to their initial value */
+function resetGame() {
+    // Shuffle cards and reset values
+    shuffleCards()
+    moves = 0
+    time = 0
+    matches = 0
+    document.getElementById("game-moves").innerHTML = moves
+    document.getElementById("game-timer").innerHTML = time
+    // Start new timer
+    timer = setInterval(() => {
+        document.getElementById("game-timer").innerHTML = ++time
+    }, 1000);
+    // Clear the game board and create new cards
     let gameboard = document.getElementById("game-board")
     gameboard.innerHTML = ""
     for (let i = 0; i < 20; i++) {
@@ -158,30 +212,68 @@ function newGame(season) {
         </div>
     </div>`
     }
-    show("game-container");
 }
 
-function resetGame() {
-    shuffleCards()
-    moves = 0
-    time = 0
-    matches = 0
-    document.getElementById("game-moves").innerHTML = moves
-    document.getElementById("game-timer").innerHTML = time
-    timer = setInterval(() => {
-        document.getElementById("game-timer").innerHTML = ++time
-    }, 1000);
+/** Shuffe the card values with the Durstenfeld algorithm
+ * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffleCards() {
+    for (let i = cardValues.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cardValues[i];
+        cardValues[i] = cardValues[j];
+        cardValues[j] = temp;
+    }
 }
 
+// Navigation functions
+
+/** Change menu to the screen with the given id */
+function changeMenu(menuId) {
+    // Find all menu elements
+    let menuElements = document.getElementById("menu-container").children
+    for (let element of menuElements) {
+        if (element.id === "game-name") {
+            // Always display the game name
+            continue
+        } else if (element.id === menuId) {
+            // Show selected menu screen
+            element.classList.remove("hidden")
+        } else {
+            // Hide other screens
+            element.classList.add("hidden")
+        }
+    }
+}
+
+/** Show an html element by id */
+function show(id) {
+    document.getElementById(id).classList.remove("hidden");
+}
+
+/** Hide an html by id */
+function hide(id) {
+    document.getElementById(id).classList.add("hidden");
+}
+
+/** Show main menu screen */
+function showMainMenu() {
+    changeMenu(MENU.MAIN)
+}
+
+/** Show the theme select screen */
 function selectTheme() {
     changeMenu(MENU.THEME);
 }
 
+/** Show the how to play screen */
 function showHowToPlay() {
     changeMenu(MENU.HOWTOPLAY)
 }
 
+/** Generate the high score list and show high score screen */
 function showHighScoreList() {
+    // Generate high score list
     let list = document.getElementById("high-score-list")
     list.innerHTML = ""
     for (let score of highScoreList) {
@@ -193,39 +285,30 @@ function showHighScoreList() {
         </li>`
 
     }
+    // Show empty high score list notification if applicable
     if (highScoreList.length === 0) {
         show("empty-high-score")
     } else {
         hide("empty-high-score")
     }
+    // Show high score screen
     changeMenu(MENU.HIGHSCORE)
 }
 
-function showMainMenu() {
-    changeMenu(MENU.MAIN)
-}
-
-function changeMenu(menuId) {
-    let menuElements = document.getElementById("menu-container").children
-    for (let element of menuElements) {
-        if (element.id === "game-name") {
-            continue
-        } else if (element.id === menuId) {
-            element.classList.remove("hidden")
-        } else {
-            element.classList.add("hidden")
-        }
-    }
-}
-
+/** Show the confirm quit dialog */
 function confirmQuit() {
     show("quit-confirm")
 }
 
+/** Quit current game */
 function quitGame() {
-    changeMenu(MENU.MAIN)
-    clearInterval(timer)
+    // Hide the dialog and stop the timer
     hide("quit-confirm")
+    clearInterval(timer)
+    // Remove played season class from the game container
+    document.getElementById("game-container").classList.remove(selectedSeason)
+    // Show the main menu screen
+    changeMenu(MENU.MAIN)
     hide("game-container")
     show("menu-container")
 }
